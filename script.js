bd68ac41146d1b125decd85821031d1494b437d8
skiergr <80352508+skiergr@users.noreply.github.com>
2023-01-02 11:26:21 -0800

    V1.1

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
-document.addEventListener('DOMContentLoaded', async function() {
-    const form = document.getElementById('question-form');
-    const input = document.getElementById('question-input');
-    const response = document.getElementById('response');
-  
-    form.addEventListener('submit', async function(event) {
-      event.preventDefault();
-  
-      try {
-        // Send the question to the OpenAI API and display the response
-        const answer = await sendQuestionToOpenAI(input.value);
-        response.textContent = 'Answer: ' + answer;
-      } catch (error) {
-        response.textContent = 'Error: ' + error.message;
-      }
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('question-form');
+  const input = document.getElementById('question-input');
+  const response = document.getElementById('response');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Send the question to the OpenAI API and display the response
+    sendQuestionToOpenAI(input.value).then(function(answer) {
+      response.textContent = 'Answer: ' + answer;
     });
   });
-  
-  async function sendQuestionToOpenAI(question) {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const endpoint = 'https://api.openai.com/v1/questions';
-    const apiKey = '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';
-  
-    const response = await fetch(proxyUrl + endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
-        'model': 'text-davinci-002',
-        'prompt': question
-      })
-    });
-  
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-  
-    const json = await response.json();
-    return json['data']['answers'][0]['text'];
-  }
-  
+});
+
+async function sendQuestionToOpenAI(question) {
+  const apiKey = 'YOUR_API_KEY';
+  const endpoint = 'https://api.openai.com/v1/questions';
+
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      'model': 'text-davinci-002',
+      'prompt': question
+    })
+  });
+  const json = await response.json();
+  return json['data']['answers'][0]['text'];
+}
