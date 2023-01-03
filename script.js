e16e4fc114f0449fd6cc8243cc778d7d33102d4e
Grady <gradyramberg@gmail.com>
2023-01-03 10:52:18 -0800

    V1.3.3
    "attempting" proxy fix(doesn't work)
    formatting

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('question-form');
   const input = document.getElementById('question-input');
   const response = document.getElementById('response');
 
-  form.addEventListener('submit', function(event) {
+  form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     // Send the question to the OpenAI API and display the response
-    sendQuestionToOpenAI(input.value).then(function(answer) {
+    sendQuestionToOpenAI(input.value).then(function (answer) {
       response.textContent = 'Answer: ' + answer;
     });
   });
@@ -15,19 +15,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
 async function sendQuestionToOpenAI(question) {
   const apiKey = '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';
-  const endpoint = 'http://api.allorigins.win/get?url=https://api.openai.com/v1/questions';
+  const endpoint =
+    'https://api.allorigins.win/raw?url=https://api.openai.com/v1/questions';
 
   const response = await fetch(endpoint, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${apiKey}`
+      Authorization: `Bearer ${apiKey}`,
     },
     body: JSON.stringify({
-      'model': 'text-davinci-002',
-      'prompt': question
-    })
-    
+      model: 'text-davinci-002',
+      prompt: question,
+    }),
   });
 
   const json = await response.json();
