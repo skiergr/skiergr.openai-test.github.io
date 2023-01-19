6e5d8982d3c59b9aa36bab274aa478c8d8eb73a6
Grady <gradyramberg@gmail.com>
2023-01-19 11:21:44 -0800

    adafds

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById('question-form');
   const input = document.getElementById('question-input');
   const response = document.getElementById('response');
 
-  form.addEventListener('submit', function (event) {
+  form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Send the question to the OpenAI API and display the response
-    sendQuestionToOpenAI(input.value).then(function (answer) {
+    sendQuestionToOpenAI(input.value).then(function(answer) {
       response.textContent = 'Answer: ' + answer;
     });
   });
 });
 
 async function sendQuestionToOpenAI(question) {
-  const response = await fetch('https://api.openai.com/v1/text-davinci/', {
+  const apiKey = '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';
+  const endpoint = 'https://api.openai.com/v1/completions';
+
+  const response = await fetch(endpoint, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer  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'
+      'Authorization': `Bearer ${apiKey}`
     },
     body: JSON.stringify({
-      'text': question
+      "model": "text-davinci-003",
+        "prompt": question,
+        "max_tokens": 64,
+        "temperature": 0.5
     })
   });
-  const data = await response.json();
-  return data.text;
+  const json = await response.json();
+  return json['data']['answers'][0]['text'];
 }
