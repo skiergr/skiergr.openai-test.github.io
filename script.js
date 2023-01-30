f53ec6d1b59d93bef7328d210e354c2beb1a8629
Grady <gradyramberg@gmail.com>
2023-01-30 10:54:32 -0800

    1

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById('question-form');
-  const input = document.getElementById('question-input');
+  const placeInput = document.getElementById('place-input');
+  const lengthInput = document.getElementById('length-input');
+  const budgetInput = document.getElementById('budget-input');
   const response = document.getElementById('response');
+
+  const valueone = placeInput.value;
+  const prompt = `plan a vacation to ${valueone} for ${lengthInput.value}. Say the hotels and and activities to do each day. Make sure to be specific and add as much detail as possible for the activities and each day's plan.`
 
   form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Send the question to the OpenAI API and display the response
-    sendQuestionToOpenAI(input.value).then(function(answer) {
-      response.textContent = 'Answer: ' + answer;
+    sendQuestionToOpenAI(prompt).then(function(answer) {
+      response.textContent = 'Propmt: ' + prompt + 'Answer: ' + answer;
     });
   });
 });
 
 async function sendQuestionToOpenAI(question) {
-  const apiKey = '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';
+  const apiKey = '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';
   const endpoint = 'https://api.openai.com/v1/completions';
 
   const response = await fetch(endpoint, {
@@ -24,13 +29,13 @@ async function sendQuestionToOpenAI(question) {
       'Authorization': `Bearer ${apiKey}`
     },
     body: JSON.stringify({
-  "model": "text-davinci-003",
-  "prompt": "question",
-  "max_tokens": 64,
-  "temperature": 0.5
+      "model": "text-davinci-003",
+      "prompt": question,
+      "max_tokens": 64,
+      "temperature": 1
 })
   });
   const json = await response.json();
   console.log(json);
-  return json.data.choices[0];
+  return json.choices[0].text;
 }
